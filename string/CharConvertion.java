c86836afd4db4a9157b32d5a66bbe7b049d9a8e7
praveennarayanan01 <praveennarayanan01@gmail.com>
2024-01-26 23:49:22 +0530

    coded CharConvertion.java

---
+++ string/CharConvertion.java
@@ -0,0 +1,14 @@
+
+class CharConvertion{
+  /*
+   * in this method i have written how to convert char
+   * to ASCII value which is integer
+   */
+  public static int charToASCII(char a){
+    int b=a;
+    return b;
+  }
+  public static void main(String[]args){
+    System.out.println(charToASCII('h'));
+  }
+}
