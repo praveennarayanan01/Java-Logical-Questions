8d8458d1ef329d8ca0e14e4fbf6d557bb5e29bb3
praveennarayanan01 <praveennarayanan01@gmail.com>
2024-01-27 22:05:18 +0530

    updated

--- fourwtechnologies/BinaryMultiply.java
+++ fourwtechnologies/BinaryMultiply.java
@@ -2,6 +2,15 @@ package fourwtechnologies;
 
 public class BinaryMultiply {
   public static void main(String[]args){
-
+    System.out.println(binaryToDecimal("1101"));
+  }
+  public static int binaryToDecimal(String a){
+  int decimalValue=0;
+  int l=a.length()-1;
+    for(int i=0;i<=a.length()-1;i++){
+      if(a.charAt(i)=='1'){
+        decimalValue= decimalValue+ (int) (1*(Math.pow(2, l)));
+      } l--;
+    } return decimalValue;
   }
 }
