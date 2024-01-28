ed6113331a7419c84d81c72ceb3f393e7de55988
praveennarayanan01 <praveennarayanan01@gmail.com>
2024-01-28 12:40:03 +0530

    Created ReplaceDuplicate.java

---
+++ fourwtechnologies/ReplaceDuplicate.java
@@ -0,0 +1,22 @@
+/*
+ * In this class we have to replace the duplicate with 0 in a Array
+ */
+package fourwtechnologies;
+
+import java.util.Arrays;
+
+public class ReplaceDuplicate {
+  public static void main(String[]args){
+    int array[]={1,2,3,4,1,5,2,1};
+    replaceDuplicate(array);
+  }
+  public static void replaceDuplicate(int[]a){
+    int j=1;
+    
+    for(int i=0;i<=a.length-2;i++){
+      for()
+    } for(int ch:a){
+        System.out.println(ch);
+    }
+  }
+}
